5e838526ab6e89169102c98796f058c483d6e696
mostafa amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2022-06-19 18:07:37 +0100

    I write checks posibilities

---
+++ registre/sign_up.php
@@ -0,0 +1,20 @@
+<?php
+/*
+    all checks : 
+    the errors will display on the div
+    name = empty
+    email = empty - contain @
+    password = empty - contain at least 8 cararcters
+    confirm password = must be identical to the original password
+
+*/
+
+
+
+
+
+
+
+
+
+?>

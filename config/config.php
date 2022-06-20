68230723fe81d1654d101ab20a48eba4146890ff
mostafa amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2022-06-20 11:10:39 +0100

    connecting to database

--- config/config.php
+++ config/config.php
@@ -1,8 +1,25 @@
 <?php
-// configuration file 
-
-
 // connecting to database 
+
+$host = "localhost";
+$user = "root";
+$port = "3307";
+
+$db = "notes_application";
+$password = "";
+
+$dsn = "mysql:host=$host;dbname=$db;$port=$port";
+
+
+try{
+    $pdo = new PDO($dsn , $user , $password);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOEXCEPTION $e)
+{
+	$e->getMessage();
+}
+
 
 
 

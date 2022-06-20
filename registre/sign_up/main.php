87a38207175e77c35eb73c36339ea2cd211b2b2c
mostafa amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2022-06-20 12:35:29 +0100

    I modify the main.php file and i added to it succes / error message

--- registre/sign_up/main.php
+++ registre/sign_up/main.php
@@ -2,7 +2,32 @@
 $title = "sign up";
 require("../../inc/header.php")
 ?>
+<style>
+.success_message{
+background-color: rgb(0, 220, 0);
+border: 1px solid #e6e6e6;
+padding: 5px;
+text-align: center;
+color: white;
+}
 
+.error_message{
+background-color: #FF0000;
+border: 1px solid #e6e6e6;
+padding: 5px;
+text-align: center;
+color: white;
+}
+</style>
+
+
+<div class="container success_message col-6 mt-5 d-none">
+	
+</div>
+
+<div class="container error_message col-6 mt-5 d-none">
+	
+</div>
 
 
 <div class="container main_div d-flex justify-content-center">
@@ -34,7 +59,7 @@ require("../../inc/header.php")
     <div class="form-group mt-2">
 	<label class="col-sm-3 control-label">Confirm Password</label>
 	<div class="col-sm-8">
-	<input type="password" id="txt_password" class="form-control" placeholder="enter password" />
+	<input type="password" id="txt_password_confirm" class="form-control" placeholder="confirm password" />
 	</div>
 	</div>
 				
@@ -42,10 +67,6 @@ require("../../inc/header.php")
 	<div class="col-sm-offset-3 mt-3">
 	<button type="submit" id="btn_register" class="btn btn-success">Register</button>
 	</div>
-	</div>
-				
-	<div class="form-group mt-2">
-		<div id="message" class="col-sm-offset-3 col-sm-6 m-t-15"></div>
 	</div>
 
 </form>
@@ -61,7 +82,25 @@ require("../../inc/header.php")
 </div>
 
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
+<script>
+$("#btn_register").click(function (e) {
+    e.preventDefault();
+
+    // get the data
+    var txt_username = $("#txt_username").val();
+    var txt_email = $("#txt_email").val();
+    var txt_password = $("#txt_password").val();
+    var txt_password_confirm = $("#txt_password_confirm").val();
+
+	
+
+
+
+})
+</script>
 <?php
 require("../../inc/footer.php")
 ?>
